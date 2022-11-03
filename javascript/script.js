cfb84b4d889d655358ac4a3eaa9b7eb473c987a1
4clean <112877373+4clean@users.noreply.github.com>
2022-11-03 21:35:36 +0700

    Create script.js

---
+++ javascript/script.js
@@ -0,0 +1,47 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+// navbar
+let parentNavbar = document.getElementById('navigation-bar');
+let banner = document.getElementById('home');
+
+window.addEventListener('scroll', () => {
+  if ( window.scrollY > banner.offsetHeight - 15) {
+    parentNavbar.style.boxShadow = "0 .5rem 1rem rgba(0,0,0,.1)";
+    parentNavbar.style.backgroundColor = "white";
+  } else {
+    parentNavbar.style.boxShadow = 'none';
+    parentNavbar.style.backgroundColor = "rgba(0,0,0,0)";
+  }
+})
+
+menu.onclick = () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 60){
+    document.querySelector('#scroll-top').classList.add('active');
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
+}
+
+// loader
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 500);
+}
+
+window.onload = fadeOut();
